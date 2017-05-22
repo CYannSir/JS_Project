a594ee64b347a5af76ab30e5d1452e4be9d45b34
CYann <wcy623209668@vip.qq.com>
2017-05-22 15:13:12 +0800

    gameDemo地图部分修改

--- gameDemo/gameLevel.js
+++ gameDemo/gameLevel.js
@@ -1,3 +1,20 @@
+/*
+*宽147格子，高47格子，实际高度为41格子；
+* "!" 地面为冰属性地
+* "x" 地面为草地属性地
+* "@" 人物初始位置
+* "o" 清图中所有怪物后，关卡传送门出现的初始位置
+* "q" 1类怪物
+* "w" 2类怪物
+* "e" 3类怪物
+* "r" 4类怪物
+* "t" 5类怪物
+* "y" 6类怪物
+* "u" 7类怪物
+* "i" 8类怪物
+* "a" 9类怪物
+* "s" 10类怪物
+*/
 var GAME_LEVELS = [
    "                                                                                                                                                         ",
    "                                                                                                                                                         ",
@@ -5,18 +22,42 @@ var GAME_LEVELS = [
    "                                                                                                                                                         ",
    "                                                                                                                                                         ",
    "                                                                                                                                                         ",
-   "                                                                  xxx                                                                                    ",
-   "                                                   xx      xx    xx!xx                                                                                   ",
-   "                                             xx                  x!!!x                                                                                   ",
-   "                                                                 xx!xx                                                                                   ",
-   "                                   xxxxx                          xxx                                                                                    ",
-   "                                                                            xx                                                                           ",
-   "  xx                                                                         x                                                                           ",
-   "  x                                                                          xxxxxxxxxxxxxxxxx                                                           ",
-   "  x                                      xxxxx                               xxxxxxxxxxxxxxxxx                                                           ",
-   "  x          xxxx                                                            x                                                                           ",
-   "  x  @       x  x                                                xxxxx       x                                                                           ",
-   "  xxxxxxxxxxxx  xxxxxxxxx!!!!!xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                                                                                                         ",
+   "                                                                            xxxxxx                                                                       ",
+   "                                                                                                                                                         ",
+   "                                                                                   xxx                                                                   ",
+   "                xx                                                                                                              		                     ",
+   "              xxxxxx                                         xxxxxxxxxxx    xxxxxxxxxxx                                     								             ",
+   "           xxxxxxxxxxxx                                                                                   						     			                         ",
+   "         xx            xx        xxxxxxxxxxx           xxxx                                                                 		                         ",
+   "       xx                xx                                                                                            									                 ",
+   "      xxxxxxxxxxxxxxxxxxxxxx                   xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                                                                          ",
+   "           xxxx    xxxx                                                                                                                                  ",
+   "           x  x    x  x          xxxxxxxxxxx                                  xxxx   xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx		       ",
+   "           xxxx    xxxx                         xxxxx                                                                                                    ",
+   "               xxxx          xxxx                                                                                                                        ",
+   "                                   !!!!!!!!!!!!!!!                                                                                                       ",
+   "    @                                                                                                                                                    ",
+   "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
    "                                                                                                                                                         ",
    "                                                                                                                                                         ",
    "                                                                                                                                                         ",
@@ -24,6 +65,8 @@ var GAME_LEVELS = [
    "                                                                                                                                                         ",
    "                                                                                                                                                         "
 ];
+
+//人物跳的高度4，宽度2
 
 if (typeof module != "undefined" && module.exports)
   module.exports = GAME_LEVELS;
